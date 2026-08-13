179e787d7dd093798806fd4f37b5a2169607280b
Kanav Jain <kanavj@gmail.com>
2026-08-13 10:10:24 -0500

    Rename capability-graph formula to ambit

---
+++ Formula/ambit.rb
@@ -0,0 +1,16 @@
+class Ambit < Formula
+  desc "The combined action space of you, your agents, and your machines"
+  homepage "https://github.com/zz-plant/ambit"
+  url "https://github.com/zz-plant/ambit/archive/refs/heads/main.tar.gz"
+  version "0.3.0"
+  license "MIT"
+  depends_on "node"
+  def install
+    system "npm", "install", "--production"
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"cli.js" => "tt"
+  end
+  test do
+    system "#{bin}/tt", "--help"
+  end
+end
